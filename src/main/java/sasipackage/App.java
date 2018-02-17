c5de2c15b3071ccdca4b649d174286499e715805
sasidhar <chinimillisasidhar@gmail.com>
2018-02-17 23:38:20 +0530

    adding pom

---
+++ src/main/java/sasipackage/App.java
@@ -0,0 +1,13 @@
+package sasipackage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
